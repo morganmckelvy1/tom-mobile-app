2add49b78f79828ac94119f1d2acedd2eb8e2603
Ajay <services.creativecodecraft@gmail.com>
2024-06-15 22:04:14 +0530

    app prod release config changes

--- src/Global/common.js
+++ src/Global/common.js
@@ -1,3 +1,1 @@
-// export const BASEURL = 'https://tomcms.app/api/v1';
-//export const BASEURL = 'https://staging.tomcms.app/api/v1';
-export const BASEURL = 'http://10.0.2.2:8000/api/v1';
+export const BASEURL = 'https://tomcms.app/api/v1';
